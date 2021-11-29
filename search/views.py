02139b60ac482f9a2a024e26182de319bd6ea8f5
SHOU6439 <shian35777@gmail.com>
2021-11-29 15:09:51 +0900

    create search template

--- search/views.py
+++ search/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from communities.models import Communities
 # Create your views here.
 class IndexView(generic.ListView):
-    template_name = 'news_posts/index.html'
+    template_name = 'search/index.html'
 
     def get_queryset(self):
         q = self.request.GET.get('q')
