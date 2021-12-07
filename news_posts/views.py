fd91173c4cd68a8fe5b4cca8f439e42abcadabd6
SHOU6439 <shian35777@gmail.com>
2021-12-07 15:48:36 +0900

    add profile detail

--- news_posts/views.py
+++ news_posts/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls.base import reverse_lazy
 from django.views import generic
 from django.views.generic.base import TemplateView
+
+from communities.models import Communities
 from .models import NewsPosts
 from .forms import CreatePostForm
 # Create your views here.
@@ -12,10 +15,14 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return NewsPosts.objects.all()
 
-class CreatePostView(generic.CreateView):
+class CreatePostView(generic.CreateView, LoginRequiredMixin):
     model = NewsPosts
     form_class = CreatePostForm
     template_name = 'news_posts/create_post.html'
     success_url = reverse_lazy('news_posts:index')
+
+    def get_queryset(self):
+        return Communities.objects.all()
+
 
 
