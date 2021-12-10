71c72baf5d9810584f1a0a43dcda61dd5ab24c0e
SHOU6439 <shian35777@gmail.com>
2021-12-10 16:25:18 +0900

    add deleteView

--- news_posts/models.py
+++ news_posts/models.py
@@ -10,7 +10,7 @@ class NewsPosts(models.Model):
         on_delete=models.CASCADE,
     )
     title = models.CharField(max_length=64, unique=False)
-    content = models.TextField(max_length=512)
+    content = models.TextField(max_length=512, null=True, blank=True)
     photo = models.ImageField(verbose_name='写真', blank=True, null=True, upload_to='images/')
     post_photo = ImageSpecField(source='photo',processors=[ResizeToFill(1080,1080)],format='JPEG',options={'quality':60})
     vote = models.IntegerField(default=0)
--- news_posts/urls.py
+++ news_posts/urls.py
@@ -5,4 +5,5 @@ app_name = 'news_posts'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('create-post/', views.CreatePostView.as_view(), name='create_post'),
+    path('delete/<int:pk>/', views.DeletePostView.as_view(), name="delete_post")
 ]
