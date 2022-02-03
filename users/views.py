8735a1a1b551154aefaa165f8f45297384c8f753
SHOU6439 <shian35777@gmail.com>
2022-02-03 13:37:17 +0900

    down_voted_postsの一覧を追加した

--- users/views.py
+++ users/views.py
@@ -96,3 +96,13 @@ class UserUpVotedPostsView(LoginRequiredMixin, generic.DetailView):
         context['up_voted_posts'] = Vote.objects.filter(voted_user=self.kwargs['pk'], flag=1).order_by("-created_at")
         context['communities_list'] = Communities.objects.filter(member=self.kwargs['pk']).order_by("-created_at")
         return context
+
+class UserDownVotedPostsView(LoginRequiredMixin, generic.DetailView):
+    model = User
+    template_name = 'users/down_voted_posts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['down_voted_posts'] = Vote.objects.filter(voted_user=self.kwargs['pk'], flag=-1).order_by("-created_at")
+        context['communities_list'] = Communities.objects.filter(member=self.kwargs['pk']).order_by("-created_at")
+        return context
