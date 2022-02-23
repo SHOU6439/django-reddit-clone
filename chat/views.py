1e48dc2d2c5b8923ff6726dc25b4a3f71a70b519
SHOU6439 <shian35777@gmail.com>
2022-02-23 16:26:56 +0900

    chat機能に他人のトーク内容が見れるという脆弱性があったので、修正した。

--- chat/views.py
+++ chat/views.py
@@ -84,12 +84,22 @@ class DMRoomDetailView(LoginRequiredMixin, generic.DetailView):
     model = DMRoom
     template_name = 'chat/dm_room_detail.html'
 
+    def get(self, request, *args, **kwargs):
+        is_author = DMRoom.objects.filter(id=self.kwargs['pk']).first().author.id
+        is_addressee = DMRoom.objects.filter(id=self.kwargs['pk']).first().addressee.id
+        if is_author == self.request.user.id or is_addressee == self.request.user.id:
+            self.object = self.get_object()
+            context = self.get_context_data(object=self.object)
+            return self.render_to_response(context)
+        else:
+            return redirect('chat:home')
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         addressee_pk = DMRoom.objects.get(id=self.kwargs['pk']).author.id
         author = User.objects.get(id=self.request.user.id)
-        addresee = User.objects.get(id=addressee_pk)
-        context['current_invite'] = DMInvite.objects.filter(invited_user=addresee, received_user=author)
+        addressee = User.objects.get(id=addressee_pk)
+        context['current_invite'] = DMInvite.objects.filter(invited_user=addressee, received_user=author)
         context['current_room'] = DMRoom.objects.get(id=self.kwargs['pk'])
         context['dm_list'] = DirectMessage.objects.filter(room=self.kwargs['pk'])
         context['dm_invites'] = DMInvite.objects.filter(received_user=self.request.user.id).order_by("-created_at")
