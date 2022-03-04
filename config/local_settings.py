dd9a0f1277b6d4a754ce3af29d83c3055414fd7c
SHOU6439 <shian35777@gmail.com>
2022-03-04 16:25:22 +0900

    DEBUG=True

---
+++ config/local_settings.py
@@ -0,0 +1,19 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'k#p)dstl)w9z$ry1u@cy1shkw$pp0sjhu-ay-ym_fq_fv0a+25' #追加
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reddit_clone',
+        'USER': 'root',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+ALLOWED_HOSTS = []
+
+DEBUG = True
--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 
@@ -179,11 +179,11 @@ try:
 except ImportError:
     pass
 
-# if not DEBUG:
-SECRET_KEY = os.environ['SECRET_KEY']
-import django_heroku #追加
-django_heroku.settings(locals()) #追加
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    SECRET_KEY = os.environ['SECRET_KEY']
+    import django_heroku #追加
+    django_heroku.settings(locals()) #追加
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
