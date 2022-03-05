3be4292b047bbb3de2ff14cde0fb4b8b57cc628d
SHOU6439 <shian35777@gmail.com>
2022-03-05 09:43:57 +0900

    本場環境のmedia_rootを指定

--- config/urls.py
+++ config/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('community/', include('communities.urls')),
     path('search/', include('search.urls')),
     path('chat/', include('chat.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
