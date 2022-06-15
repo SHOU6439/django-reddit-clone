f9e42964b8314d90f7824561f88b4b2b060fa35f
SHOU6439 <shian35777@gmail.com>
2022-06-15 15:21:09 +0900

    user_down_voted_post_viewを元に戻した

--- users/views/user_down_voted_post_view.py
+++ users/views/user_down_voted_post_view.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from news_posts.models import NewsPosts
+from news_posts.models import Vote
 from django.contrib.auth import get_user_model
 from django.views import generic
 from communities.models import Communities
@@ -13,6 +13,6 @@ class UserDownVotedPostsView(LoginRequiredMixin, generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['down_voted_posts'] = NewsPosts.objects.filter(voted_user=self.kwargs['pk']).order_by("-created_at")
+        context['down_voted_posts'] = Vote.objects.filter(voted_user=self.kwargs['pk'], flag=-1).order_by("-created_at")
         context['communities_list'] = Communities.objects.filter(member=self.kwargs['pk']).order_by("-created_at")
         return context
