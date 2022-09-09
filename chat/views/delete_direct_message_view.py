2aeb9a3fea46cb7516201a98e9a30bdc732077fa
SHOU6439 <shian35777@gmail.com>
2022-09-09 15:34:34 +0900

    DeleteDirectMessageViewを実装した

---
+++ chat/views/delete_direct_message_view.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls.base import reverse_lazy
+from django.views import generic
+from chat.models import DirectMessage, DMRoom
+from django.db.models import Model
+from django.shortcuts import get_object_or_404
+from django.urls.base import reverse
+
+class DeleteDirectMessageView(LoginRequiredMixin, generic.DeleteView):
+    model: Model = DirectMessage
+    def get_success_url(self) -> str:
+        # replayの対象であるコメントの対象であるポストのpkを取得する
+        direct_message_pk = self.kwargs['pk']
+        direct_message = get_object_or_404(DMRoom, pk=direct_message_pk)
+        dm_room_pk = direct_message.room.id
+        return reverse('news_posts:dm_room_detail', kwargs={'pk': dm_room_pk})
